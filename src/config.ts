a4326db3b093eb16f613a2c8d73e46e51a830026
atorber <atorber@163.com>
2023-08-09 13:44:58 +0800

    增加角色自定义支持

--- src/config.ts
+++ src/config.ts
@@ -166,3 +166,8 @@ export {
   storeHistory,
   baseConfig,
 }
+
+// "wechaty-puppet-padlocal": "^1.20.1",
+// "wechaty-puppet-wechat4u": "^1.13.15",
+// "wechaty-puppet-wechat": "^1.18.4",
+// "typechat": "^0.0.10",
